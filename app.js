cc4c3ce2cfa8a05f8fa96a2ffa6b2bdf1ab08de3
Igor123fight <94051391+Igor123fight@users.noreply.github.com>
2022-11-25 11:30:37 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+let viber = document.querySelector('#viber')
+let telegram = document.querySelector('#telegram')
+let token = 'https://testikigor.bitrix24.ru/rest/1/idadkvdihkocven8/'
+let method = "crm.deal.get" 
+let urlPage = document.location.href
+console.log(urlPage);
