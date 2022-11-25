0a86f97570ef2a5370a3460676a24e746193e6e7
Igor123fight <94051391+Igor123fight@users.noreply.github.com>
2022-11-25 11:42:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ let viber = document.querySelector('#viber')
 let telegram = document.querySelector('#telegram')
 let token = 'https://testikigor.bitrix24.ru/rest/1/idadkvdihkocven8/'
 let method = "crm.deal.get" 
-let urlPage = document.location.href
+let urlPage = document.location.href.pathname
 
-for(var pair of urlPage.searchParams.entries()) {
-   console.log(pair[0]+ ', '+ pair[1]); 
-}
 
 console.log(urlPage);
