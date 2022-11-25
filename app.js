ef3e0261c9aaa8b95c3f992bf1737800036e1ff9
Igor123fight <94051391+Igor123fight@users.noreply.github.com>
2022-11-25 11:40:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,4 +3,9 @@ let telegram = document.querySelector('#telegram')
 let token = 'https://testikigor.bitrix24.ru/rest/1/idadkvdihkocven8/'
 let method = "crm.deal.get" 
 let urlPage = document.location.href
+
+for(var pair of urlPage.searchParams.entries()) {
+   console.log(pair[0]+ ', '+ pair[1]); 
+}
+
 console.log(urlPage);
